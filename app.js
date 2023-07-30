be38c65b11690aae663edefbf7df349eb43f481d
dingbum73 <dingbum73@gmail.com>
2023-07-30 15:12:23 +0800

    feat: compelte CRUD

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
+const bodyParsr = require('body-parser')
 const routes = require('./routes/index')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+const usePassport = require('./config/passport')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 // setting template engine
 app.engine('hbs', exphbs.create({ defaultLayout: 'main', extname: '.hbs' }).engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
-app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(bodyParsr.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+usePassport(app)
+
+// 設定middleware ，在passport之後
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
+
+
 app.use(routes)
+
+
 
 
 app.listen(port, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
 
-router.use('/', (req, res) => {
-  res.render('index')
+router.get('/', (req, res) => {
+  const UserId = req.user.id
+  return Todo.findAll({
+    where: { UserId },
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 module.exports = router
